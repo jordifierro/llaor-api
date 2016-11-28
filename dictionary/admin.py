3501dfacf6185681dcd84f56b1a878a1f009a536
jordifierro <jordifierromulero@gmail.com>
2016-11-28 22:42:26 +0100

    Add search_fields and list_filters to the admin

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -3,5 +3,7 @@ from dictionary.models import Definition
 
 class DefinitionAdmin(admin.ModelAdmin):
     list_display = ['word', 'meaning', 'extra', 'synonyms', 'related', 'origin', 'public'] 
+    search_fields = ['word', 'meaning', 'extra', 'origin', 'synonyms', 'related', 'source']
+    list_filter = ['public', 'source', 'origin']
 
 admin.site.register(Definition, DefinitionAdmin)
