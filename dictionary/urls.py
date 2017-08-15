828736324c83a913f48fdcebc9290194710850e9
jordifierro <jordifierromulero@gmail.com>
2017-08-15 16:26:58 +0200

    Remove /dictionary path from /words urls

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -3,6 +3,6 @@ from llaor.views import ViewWrapper
 from dictionary.factories import DictionaryViewFactory, WordViewFactory
 
 urlpatterns = [
-    url(r'^dictionary/words/$', ViewWrapper.as_view(factory=DictionaryViewFactory), name='dictionary'),
-    url(r'^dictionary/words/(?P<word>.+)$', ViewWrapper.as_view(factory=WordViewFactory), name='word'),
+    url(r'^words/$', ViewWrapper.as_view(factory=DictionaryViewFactory), name='words-list'),
+    url(r'^words/(?P<word>.+)$', ViewWrapper.as_view(factory=WordViewFactory), name='word'),
 ]
